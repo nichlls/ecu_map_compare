2b1cba025a3588fd9c21dc400eece5865c3cd41f
nichlls <107227159+nichlls@users.noreply.github.com>
2025-10-21 21:25:12 +0100

    Update formatting

--- ecu_map_compare/Models/EcuSettingsItems.cs
+++ ecu_map_compare/Models/EcuSettingsItems.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ecu_map_compare.Models
+﻿namespace ecu_map_compare.Models
 {
     public class EcuSettingsItem
     {
